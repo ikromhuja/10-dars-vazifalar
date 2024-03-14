eeae6c4565065234d9c3c7e08d3abdcd51a357c9
ikrom marupov <ikrom@gmail.com>
2024-03-14 22:05:08 +0500

    homework 10

---
+++ css style o'zgartirish/app.js
@@ -0,0 +1,9 @@
+const heading = document.querySelector("h1");
+
+// heading.setAttribute('style', 'margin: 50px')
+console.log(heading.style);
+heading.style.margin = "50px";
+heading.style.color = "crimson";
+heading.style.fontSize = "50px";
+heading.style.margin = "";
+heading.style.backgroundColor = "#ccc";
